bd6f7dcaf12f5a381a311a05f2d6fcd04110fefb
Federico Hess <fedehess@gmail.com>
2013-07-16 14:24:26 -0600

    Added Capistrano deployment scripts and Capfile

--- config/deploy/dev.rb
+++ config/deploy/dev.rb
@@ -1,4 +1,4 @@
-server 'canvas-lms', :app, :web, :primary => true
+server 'localhost', :app, :web, :primary => true
 set :rails_env,		"development"
 set :deploy_env,	"development"
 set :bundle_without,  	[:production, :test]
