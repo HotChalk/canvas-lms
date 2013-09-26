0886bd74ab57c83c1aabbf571f5fa8153a4bd4ea
Julio Arias <julio.arias@edify.cr>
2013-09-26 17:52:02 +0200

    TL-124 change terms of service info and remove instructure notifiacations option

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -941,10 +941,10 @@ module ApplicationHelper
     # may be overridden by a plugin
     @agree_to_terms ||
     t("#user.registration.agree_to_terms_and_privacy_policy",
-      "You agree to the *terms of use* and acknowledge the **privacy policy**.",
+      "You acknowledge the **privacy policy**.",
       wrapper: {
         '*' => link_to('\1', @domain_root_account.terms_of_use_url, target: '_blank'),
-        '**' => link_to('\1', @domain_root_account.privacy_policy_url, target: '_blank')
+        '**' => link_to('\1', "http://www.hotchalkmediagroup.com/privacy-policy/", target: '_blank')
       }
     )
   end
