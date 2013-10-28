1d6bf173b50183bbded581a9a026912dff9ddba3
Federico Hess <fedehess@gmail.com>
2013-10-28 17:48:52 -0600

    TL-160: Privacy Policy at creating a user account does not correspond to Hotchalk Privacy Policy

--- app/models/account.rb
+++ app/models/account.rb
@@ -237,7 +237,7 @@ class Account < ActiveRecord::Base
   end
 
   def privacy_policy_url
-    Setting.get_cached('privacy_policy_url', 'http://www.instructure.com/policies/privacy-policy-instructure')
+    Setting.get_cached('privacy_policy_url', 'http://www.hotchalk.com/privacy-policy/')
   end
 
   def terms_required?
