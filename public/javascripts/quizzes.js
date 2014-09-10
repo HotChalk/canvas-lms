ab24f3165a07c54b56c64ab54eb1339a623d7a64
Javier Camacho <camacho.fj@gmail.com>
2014-09-10 09:52:53 -0600

    CNS-666: When a user attempted to create a Quiz received Unexpected error: ID 28718

--- public/javascripts/quizzes.js
+++ public/javascripts/quizzes.js
@@ -1856,6 +1856,16 @@ define([
       $form.attr('action', $question.find(".update_question_url").attr('href'))
         .attr('method', 'POST')
         .find('.submit_button').text(I18n.t('buttons.update_question', 'Update Question'));
+      $form.find('.submit_button').click(function(event){
+        var text = $formQuestion.find(".question_content").editorBox('get_code');
+        if(text.length < 16384) {
+          $form.find(".redactor_box").errorBox().remove();
+          $form.submit();
+        } else {
+          var offset = $form.find(".redactor_box").errorBox(I18n.t('errors.max_length_exceeded', "This field exceeds its maximum length")).offset();
+          $('html,body').scrollTo({top: offset.top, left:0});
+        }
+      });
       $form.find(":input:visible:first").focus().select();
       $("html,body").scrollTo({top: $form.offset().top - 10, left: 0});
       setTimeout(function() {
