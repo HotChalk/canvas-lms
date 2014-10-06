f1250aacf2bb6206502341c8ca2373e7e0dab863
Federico Hess <fedehess@gmail.com>
2014-10-05 19:14:34 -0600

    CNS-717: "Failed validating activity / format error" is shown when trying to preview a quiz from CL

--- vendor/plugins/learnosity/init.rb
+++ vendor/plugins/learnosity/init.rb
@@ -19,6 +19,7 @@ Rails.configuration.to_prepare do
   }
   ActionView::Base.send(:include, QuizzesHelperLearnosity)
   require File.join(File.dirname(__FILE__), 'app/models/assessment_question')
+  require File.join(File.dirname(__FILE__), 'app/models/importers/assessment_question_importer')
   require File.join(File.dirname(__FILE__), 'lib/qti/learnosity_interaction')
   require File.join(File.dirname(__FILE__), 'lib/qti/assessment_item_converter_learnosity')
   require File.join(File.dirname(__FILE__), 'app/controllers/quizzes/quizzes_controller')
