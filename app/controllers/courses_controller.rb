4a886698b99aa8d2fe933b7237425553d8f7c7ec
Javier Camacho <camacho.fj@gmail.com>
2014-10-13 11:15:22 -0600

    CNS-739: "Unexpected error, ID: 293173" is shown when trying to edit the Course Details
    Error was caused by account program relationship being null

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1703,10 +1703,14 @@ class CoursesController < ApplicationController
         @course.enrollment_term = enrollment_term if enrollment_term && enrollment_term != @course.enrollment_term
       end
 
-      account_program_id = params[:course].delete(:account_program_id)
-      if account_program_id && @course.root_account.grants_right?(@current_user, session, :manage_courses)
-        account_program = @course.root_account.account_programs.find(account_program_id)
-        @course.account_program = account_program if account_program && account_program != @course.account_program
+      account_program_id = params[:course].delete(:account_program_id)      
+      if @course.root_account.grants_right?(@current_user, session, :manage_courses)
+        if account_program_id != "no_program"
+          account_program = @course.root_account.account_programs.find(account_program_id)
+          @course.account_program = account_program if account_program && account_program != @course.account_program
+        else
+          @course.account_program = nil
+        end
       end
 
       if params[:course].has_key? :grading_standard_id
