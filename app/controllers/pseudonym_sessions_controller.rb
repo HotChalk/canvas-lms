21f7eed55ca2e9b064b3b9a05bf33c5982bc3319
Federico Hess <fedehess@gmail.com>
2014-11-08 12:14:08 -0600

    CNS-778: Improve SSO process for users

--- app/controllers/pseudonym_sessions_controller.rb
+++ app/controllers/pseudonym_sessions_controller.rb
@@ -796,16 +796,29 @@ class PseudonymSessionsController < ApplicationController
     # making them unable to login.
     params[:prelogin][:unique_id].try(:strip!)
 
-    # find the pseudonym associated to the supplied email address
-    communication_channel = CommunicationChannel.active.email.by_path(params[:prelogin][:unique_id]).first
-    if communication_channel.nil? || communication_channel.pseudonym.nil?
+    # find the user associated to the supplied email address
+    possible_users = []
+    Account.root_accounts.each do |root_account|
+      user_list = UserList.new(params[:prelogin][:unique_id], :root_account => root_account, :search_method => :closed).users
+      possible_users.concat(user_list)
+      break unless user_list.empty?
+    end
+    user = (possible_users ? (possible_users.uniq.first) : nil)
+
+    # find the pseudonym associated to the user, giving preference to a matching unique id. If a matching unique id is not found,
+    # fall back to the first active pseudonym for the user
+    if user
+      pseudonym = user.pseudonyms.active.select { |p| p.unique_id == params[:prelogin][:unique_id] }.first
+      pseudonym = user.pseudonyms.active.first unless pseudonym
+    end
+    unless user && pseudonym
       render :json => {:errors => {:unique_id => t('errors.no_matching_user', "HotChalk Ember doesn't have an account for user: %{user}", :user => params[:prelogin][:unique_id])}}, :status => :not_found
       return
     end
 
     # check authentication type for the pseudonym's root account
     response = {}
-    root_account = Account.find(communication_channel.pseudonym.root_account_id)
+    root_account = Account.find(pseudonym.root_account_id)
     aac = root_account.account_authorization_config
     if aac.nil?
       response[:auth_type] = 'canvas'
