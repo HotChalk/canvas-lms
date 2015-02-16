4cfc0a5347df8fa30928d2603321645a9892eb38
Javier Camacho <camacho.fj@gmail.com>
2015-02-16 15:05:50 -0600

    Add extra validations for assignments with no course section

--- lib/section_assignment.rb
+++ lib/section_assignment.rb
@@ -1,7 +1,7 @@
 module SectionAssignment
 
   def assigned_to_section?
-    if self.is_a?(Assignment) || self.is_a?(DiscussionTopic) #|| Quizzes::Quiz.class_names.include?(self.class_name)
+    if (self.is_a?(Assignment) || self.is_a?(DiscussionTopic)) && self[:course_section_id] #|| Quizzes::Quiz.class_names.include?(self.class_name)
       self.course_section_id.present?
     else
       false
