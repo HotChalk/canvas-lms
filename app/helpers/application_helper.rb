e58470680859ce721c3ce13e420ea3cf999ad92f
Federico Hess <fedehess@gmail.com>
2015-06-24 13:59:33 -0600

    Included Google Tag Manager as an external web tool

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -931,4 +931,21 @@ module ApplicationHelper
     snippet.html_safe
   end
 
+  def include_google_tag_manager
+    snippet = ""
+    @inspectlet_config ||= ConfigFile.load('external_web_tools')
+    if @inspectlet_config['enable_google_tag_manager']
+      snippet = %[
+<noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-MC7WJC"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-MC7WJC');</script>
+    ]
+    end
+    snippet.html_safe
+  end
+
 end
