41bc5ffe309bdaf92fdce88a245f4faf392d3ce5
Federico Hess <fedehess@gmail.com>
2016-02-10 11:31:13 -0600

    Updated Google Tag Manager embed code

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -999,13 +999,13 @@ module ApplicationHelper
     @external_web_tools_config ||= ConfigFile.load('external_web_tools')
     if @external_web_tools_config['enable_google_tag_manager']
       snippet = %[
-        <noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-MC7WJC"
+        <noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-K3GZPD"
         height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
         <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
         new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
         j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         '//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-MC7WJC');</script>
+        })(window,document,'script','dataLayer','GTM-K3GZPD');</script>
     ]
     end
     snippet.html_safe
