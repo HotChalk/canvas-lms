1539cbb044376c029cf223c82cb3e7321364d6bc
Raul Miranda <raul.miranda@edify.cr>
2016-07-20 17:33:20 -0600

    testingbot testing localhost

--- spec/selenium/test_setup/selenium_driver_setup.rb
+++ spec/selenium/test_setup/selenium_driver_setup.rb
@@ -131,7 +131,7 @@ module SeleniumDriverSetup
     Selenium::WebDriver.for(
         :remote,
         :url => "http://#{$selenium_config[:testingbot_key]}:" +
-            "#{$selenium_config[:testingbot_secret]}@hub.testingbot.com:4444/wd/hub",
+            "#{$selenium_config[:testingbot_secret]}localhost:8087/wd/hub",
         :desired_capabilities => caps)
   end
 
