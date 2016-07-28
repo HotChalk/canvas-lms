8c160b5fd24aa3fd165c27ec48ad298b418615eb
Juan Soto <juan.soto@edify.cr>
2016-07-28 15:51:32 -0600

    feat(LMS-1739):enable changes for use new help UI

--- app/models/account.rb
+++ app/models/account.rb
@@ -1420,7 +1420,7 @@ class Account < ActiveRecord::Base
       tabs << { :id => TAB_QUESTION_BANKS, :label => t('#account.tab_question_banks', "Question Banks"), :css_class => 'question_banks', :href => :account_question_banks_path } if user && self.grants_right?(user, :manage_assignments)
       tabs << { :id => TAB_SUB_ACCOUNTS, :label => t('#account.tab_sub_accounts', "Sub-Accounts"), :css_class => 'sub_accounts', :href => :account_sub_accounts_path } if manage_settings
       tabs << { :id => TAB_RESOURCES, :label => t('#account.tab_resources', "Resources"), :css_class => 'resources', :href => :account_resources_path } if manage_settings
-      # tabs << { :id => TAB_HELP_SETUP_LINKS, :label => t('#account.tab_help_setup', "Help Setup"), :css_class => 'help_setup', :href => :account_helpsetup_path} if manage_settings
+      tabs << { :id => TAB_HELP_SETUP_LINKS, :label => t('#account.tab_help_setup', "Help Setup"), :css_class => 'help_setup', :href => :account_helpsetup_path} if manage_settings
       tabs << { :id => TAB_FACULTY_JOURNAL, :label => t('#account.tab_faculty_journal', "Faculty Journal"), :css_class => 'faculty_journal', :href => :account_user_notes_path} if self.enable_user_notes && user && self.grants_right?(user, :manage_user_notes)
       tabs << { :id => TAB_TERMS, :label => t('#account.tab_terms', "Terms"), :css_class => 'terms', :href => :account_terms_path } if self.root_account? && manage_settings
       tabs << { :id => TAB_AUTHENTICATION, :label => t('#account.tab_authentication', "Authentication"), :css_class => 'authentication', :href => :account_authentication_providers_path } if self.root_account? && manage_settings
