6b5c248e1a5e606da636eeacb006210c8c7d7864
Juan Soto <juan.soto@edify.cr>
2016-11-03 09:43:47 -0600

    fix(LMS-2099): fixed issue LMS-2099 Add inspectlet code to each respective single tenent environments

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1053,15 +1053,19 @@ module ApplicationHelper
     snippet = ""
     @external_web_tools_config ||= ConfigFile.load('external_web_tools')
     if @external_web_tools_config['enable_inspectlet']
-      snippet = %[ <script type="text/javascript" id="inspectletjs">
-        window.__insp = window.__insp || [];
-        __insp.push(['wid', 1695757460]);
-        (function() {
-          function ldinsp(){if(typeof window.__inspld != "undefined") return; window.__inspld = 1; var insp = document.createElement('script'); insp.type = 'text/javascript'; insp.async = true; insp.id = "inspsync"; insp.src = ('https:' == document.location.protocol ? 'https' : 'http') + '://cdn.inspectlet.com/inspectlet.js'; var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(insp, x); };
-          setTimeout(ldinsp, 500); document.readyState != "complete" ? (window.attachEvent ? window.attachEvent('onload', ldinsp) : window.addEventListener('load', ldinsp, false)) : ldinsp();
-        })();
-      </script>
-    ]
+      if @domain_root_account.settings[:inspectlet_script]
+        snippet = @domain_root_account.settings[:inspectlet_script]
+      else
+        snippet = %[ <script type="text/javascript" id="inspectletjs">
+            window.__insp = window.__insp || [];
+            __insp.push(['wid', 1695757460]);
+            (function() {
+              function ldinsp(){if(typeof window.__inspld != "undefined") return; window.__inspld = 1; var insp = document.createElement('script'); insp.type = 'text/javascript'; insp.async = true; insp.id = "inspsync"; insp.src = ('https:' == document.location.protocol ? 'https' : 'http') + '://cdn.inspectlet.com/inspectlet.js'; var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(insp, x); };
+              setTimeout(ldinsp, 500); document.readyState != "complete" ? (window.attachEvent ? window.attachEvent('onload', ldinsp) : window.addEventListener('load', ldinsp, false)) : ldinsp();
+            })();
+          </script>
+        ]  
+      end
     end
     snippet.html_safe
   end
